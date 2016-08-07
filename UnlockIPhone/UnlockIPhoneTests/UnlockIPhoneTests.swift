d86005bfd96635bd3524f5cff791cf5696c16525
saitjr <saitjr@saitjrs-MBP.lan>
2016-08-07 23:24:19 +0800

    add unlock iphone animation

---
+++ UnlockIPhone/UnlockIPhoneTests/UnlockIPhoneTests.swift
@@ -0,0 +1,36 @@
+//
+//  UnlockIPhoneTests.swift
+//  UnlockIPhoneTests
+//
+//  Created by saitjr on 8/7/16.
+//  Copyright © 2016 saitjr. All rights reserved.
+//
+
+import XCTest
+@testable import UnlockIPhone
+
+class UnlockIPhoneTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
