6c0fd39e43c0e2f81d2b8ef38201d114400ef7a5
saitjr <saitjr@saitjrs-MBP.lan>
2016-08-08 22:50:41 +0800

    add UnlockIPhone animation

--- UnlockIPhone/UnlockIPhone/ViewController.swift
+++ UnlockIPhone/UnlockIPhone/ViewController.swift
@@ -13,13 +13,50 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        setupUI()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
+extension ViewController {
+    private func setupUI() {
+        view.backgroundColor = .blackColor()
+        
+        let containerView = UIView()
+        containerView.bounds = CGRect(x: 0, y: 0, width: 300, height: 80)
+        containerView.center = view.center
+        view.addSubview(containerView)
+        
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = containerView.bounds
+        gradientLayer.colors = [UIColor.blackColor().CGColor, UIColor.whiteColor().CGColor, UIColor.blackColor().CGColor]
+        gradientLayer.locations = [0.25, 0.5, 0.75]
+        gradientLayer.startPoint = CGPoint(x: 0, y: 0.5)
+        gradientLayer.endPoint = CGPoint(x: 1, y: 0.5)
+        containerView.layer.addSublayer(gradientLayer)
+        
+        let text: NSString = "Saitjr"
+        let style = NSMutableParagraphStyle()
+        style.alignment = .Center
+        let textAttribute: [String: AnyObject] = [NSFontAttributeName: UIFont(name: "HelveticaNeue-Thin", size: 30)!, NSParagraphStyleAttributeName: style]
+        UIGraphicsBeginImageContextWithOptions(view.bounds.size, false, 0)
+        text.drawInRect(containerView.bounds, withAttributes: textAttribute)
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        let textLayer = CALayer()
+        textLayer.frame = view.bounds.offsetBy(dx: 0, dy: 20)
+        textLayer.contents = image.CGImage
+        gradientLayer.mask = textLayer
+        
+        gradientLayer.addAnimation(animation(), forKey: "LocationAnimation")
+    }
+    
+    private func animation() -> CABasicAnimation {
+        let animation = CABasicAnimation(keyPath: "locations")
+        animation.fromValue = [0.0, 0.0, 0.25]
+        animation.toValue = [0.75, 1.0, 1.0]
+        animation.duration = 3.0
+        animation.repeatCount = Float.infinity
+        return animation
+    }
+}
